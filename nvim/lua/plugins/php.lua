8e73b393ea214c2a65775a81fb4a8ccf2a481af6
Jean-Marc Strauven <jms@grazulex.be>
2025-04-20 20:59:43 +0200

    add nvim config

---
+++ nvim/lua/plugins/php.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { "pint", "php_cs_fixer" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        php = {},
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = { "V13Axel/neotest-pest" },
+    opts = { adapters = { "neotest-pest" } },
+  },
+}
